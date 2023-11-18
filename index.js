c066f0ba78ccef9702bdebea043e7e357c0b1127
seasons986 <annalin12345@gmail.com>
2023-11-18 03:13:05 -0500

    gaveeee uppp

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ window.addEventListener("load", () => {
     return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
   }
   const container = document.getElementById("container");
-  let garden = "";
+  // let garden = "";
   function createElement(id,text) {
     if (Math.random()>0.8) return;
     const pre = document.createElement("pre");
@@ -13,9 +13,10 @@ window.addEventListener("load", () => {
     const left = getRandomInt(-100,window.innerWidth-100);
     const top = getRandomInt(-100,window.innerHeight-100);
     pre.style.left = left+"px";
+    pre.style.top = top+"px";
     pre.style.fontSize = getRandomInt(5,18)+"px";
-    const cactus = `id-${id},${left}**`;
-    garden+=cactus;
+    // const cactus = `id-${id},${left}**`;
+    // garden+=cactus;
     container.appendChild(pre);
   }
   // incredible ascii art from -- https://www.asciiart.eu/plants/cactus
@@ -27,11 +28,12 @@ window.addEventListener("load", () => {
 
   document.body.onclick = (e) => {
     e.preventDefault();
-    console.log(garden);
-    const text = `<!DOCTYPE html><html> <head> <title>janet</title> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1"> <link rel="stylesheet" href="https://iguannalin.github.io/janet/index.css"/> </head> <body> <div id="container" data-garden=${btoa(garden)}></div><script>window.addEventListener("load", ()=>{const container=document.getElementById("container"); const garden=atob(container.dataset.garden); function goFurther(){garden.split("**").forEach((el)=>{const pieces=el.split(","); if (pieces.length<1) return; document.getElementById(pieces[0]).style.left=+(pieces[1])+1+"px";}); goFurther();}});</script> </body></html>`;
-    const blob = new Blob([text], {type: "text/html"});
-    const blobUrl = URL.createObjectURL(blob);
-    window.open(blobUrl, '_blank');
-    window.URL.revokeObjectURL(blobUrl);
+    location.reload();
+    // console.log(garden);
+    // const text = `<!DOCTYPE html><html> <head> <title>janet</title> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1"> <link rel="stylesheet" href="https://iguannalin.github.io/janet/index.css"/> </head> <body> <div id="container" data-garden=${btoa(garden)}></div><script>window.addEventListener("load", ()=>{const container=document.getElementById("container"); const garden=atob(container.dataset.garden); function goFurther(){garden.split("**").forEach((el)=>{const pieces=el.split(","); if (pieces.length<1) return; document.getElementById(pieces[0]).style.left=+(pieces[1])+1+"px";}); goFurther();}});</script> </body></html>`;
+    // const blob = new Blob([text], {type: "text/html"});
+    // const blobUrl = URL.createObjectURL(blob);
+    // window.open(blobUrl, '_blank');
+    // window.URL.revokeObjectURL(blobUrl);
   }
 });
